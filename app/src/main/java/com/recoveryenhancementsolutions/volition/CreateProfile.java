1d8a8396db81e64a03d9dab1a6e4f4ac6cb55765
Stephanie Connell <connells@duq.edu>
2019-03-16 18:11:05 -0400

    Completed and tested UI for entering demographic information.

---
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/CreateProfile.java
@@ -0,0 +1,14 @@
+package com.recoveryenhancementsolutions.volition;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/** Class for testing create_profile.xml */
+public class CreateProfile extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.create_profile);
+  }
+
+}
