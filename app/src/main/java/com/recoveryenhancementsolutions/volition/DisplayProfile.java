fd775ce981bf656156a8833e3d2ff0a908d49919
Stephanie Connell <connells@duq.edu>
2019-03-16 19:39:42 -0400

    Completed screen for displaying the information entered.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/DisplayProfile.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/DisplayProfile.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /** Class for running only create_profile.xml */
-public class CreateProfile extends AppCompatActivity {
+public class DisplayProfile extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.create_profile);
+    setContentView(R.layout.display_profile);
   }
 
 }
