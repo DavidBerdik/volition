7d5778a4116eaa34336dbed0e73f574bd1e217f9
Rahul Ghai <ghair@duq.edu>
2019-03-17 13:50:15 -0400

    saving work done so far

--- app/src/main/java/com/recoveryenhancementsolutions/volition/DemographicDataDAO.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/DemographicDataDAO.java
@@ -1,10 +1,17 @@
 package com.recoveryenhancementsolutions.volition;
 
+
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+
+import java.util.List;
 
 @Dao
 public interface DemographicDataDAO {
     @Insert
     void insertActivity(DemographicDataEntity demographicDataEntity);
+    @Delete
+    void deleteActivity(DemographicDataEntity demographicDataEntity);
+
 }
