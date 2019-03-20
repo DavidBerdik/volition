a4de8ad834fc7dca8ecd36e7147a4f49d514efff
John Natale <natalej1@duq.edu>
2019-03-20 19:06:24 -0400

    Add basic database call.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/HomeActivityViewModel.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/HomeActivityViewModel.java
@@ -2,6 +2,8 @@ package com.recoveryenhancementsolutions.volition;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import java.util.Date;
 
 /**
  * ViewModel for the "HomeActivity" class. Retrieves the number of days clean from the database.
@@ -16,6 +18,15 @@ public class HomeActivityViewModel extends AndroidViewModel {
   public HomeActivityViewModel(final Application application) {
     super(application);
     db = VolitionDatabase.getDatabase(this.getApplication());
+  }
+
+  /**
+   * Retrieves the last date clean as stored in the database.
+   *
+   * @return A LiveData object containing a String representing the last listed date of being clean.
+   */
+  public LiveData<String> getLastCleanDate() {
+    return db.demographicDataDAO().queryLastCleanDate();
   }
 
   private VolitionDatabase db;
