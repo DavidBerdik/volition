aff8a014b51fe4f712ec85f3826c189d12b5931b
Noah Greenberg <noah@z98.co>
2019-03-21 18:19:07 -0400

    Removed unused import.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/ViewActivitiesActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/ViewActivitiesActivity.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class ViewActivitiesActivity extends AppCompatActivity {
 
