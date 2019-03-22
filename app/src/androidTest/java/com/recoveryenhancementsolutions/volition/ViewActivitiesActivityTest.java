6ad0b24a3f1b7912cb950a3012d86273b3f32ad7
Noah Greenberg <noah@z98.co>
2019-03-21 22:58:18 -0400

    Finished test

--- app/src/androidTest/java/com/recoveryenhancementsolutions/volition/ViewActivitiesActivityTest.java
+++ app/src/androidTest/java/com/recoveryenhancementsolutions/volition/ViewActivitiesActivityTest.java
@@ -1,20 +1,12 @@
 package com.recoveryenhancementsolutions.volition;
 
-import static org.junit.Assert.assertEquals;
-
-import android.arch.lifecycle.ViewModelProviders;
-import android.arch.persistence.room.Room;
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
-import com.recoveryenhancementsolutions.volition.utilities.LiveDataTestUtility;
+import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,14 +27,29 @@ public class ViewActivitiesActivityTest {
   @Test
   public void testActivityUpdating() {
     Calendar cal = Calendar.getInstance();
-    ArrayList<String> sampleDescs = new ArrayList<String>();
-    for (int i = 0; i < 7; i++) {
-      sampleDescs.add("Sample " +i + " days ago 1");
-      sampleDescs.add("Sample " +i + " days ago 2");
+    ArrayList<String> sampleDesc = new ArrayList<String>();
+    ArrayList<TextView> textViews = new ArrayList<TextView>();
 
-      activityTestRule.getActivity().updateDayActivities(cal, sampleDescs);
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_today));
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_yesterday));
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_2_days));
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_3_days));
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_4_days));
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_5_days));
+    textViews.add((TextView) activityTestRule.getActivity().findViewById(R.id.details_6_days));
+
+    for (int i = 0; i < 7; i++) {
+      sampleDesc.add("Sample" + i);
+      activityTestRule.getActivity().updateDayActivities(cal, sampleDesc);
+      String value = textViews.get(i).getText().toString();
+
+      Log.i("UserActivityViewModelTest",
+          "Label " + i + "; expect:\"" + sampleDesc.get(0) + "\" got:\"" + value + '"');
+
+      Assert.assertEquals(sampleDesc.get(0), value);
+      sampleDesc.clear();
+
       cal.add(Calendar.DAY_OF_MONTH, -1);
-      sampleDescs.clear();
     }
   }
 }
