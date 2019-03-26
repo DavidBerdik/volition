3876c563ef0cf5e0f58a88bc2304eaaa879e9dda
Abdulloh Yoldoshev <abdullohyoldoshev@Abdullohs-MacBook-Pro.local>
2019-03-26 15:10:13 -0400

    Made a change so that sample text gets entered in from the activity rather than the xml strings

--- app/src/main/java/com/recoveryenhancementsolutions/volition/PlanActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/PlanActivity.java
@@ -2,6 +2,7 @@ package com.recoveryenhancementsolutions.volition;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
 public class PlanActivity extends AppCompatActivity {
@@ -10,6 +11,24 @@ public class PlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+        String sampletext = "Visit 1 Visit 1";
+        TextView textView = (TextView) findViewById(R.id.textview_day_1);
+        textView.setText(sampletext);
+        textView = findViewById(R.id.textview_day_2);
+        textView.setText(sampletext);
+        textView = findViewById(R.id.textview_day_3);
+        textView.setText(sampletext);
+        textView = findViewById(R.id.textview_day_4);
+        textView.setText(sampletext);
+        textView = findViewById(R.id.day_of_week_1);
+        textView.setText("M");
+        textView = findViewById(R.id.day_of_week_2);
+        textView.setText("T");
+        textView = findViewById(R.id.day_of_week_3);
+        textView.setText("W");
+        textView = findViewById(R.id.day_of_week_4);
+        textView.setText("T");
+
     }
 
 }
