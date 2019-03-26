103786c36ad69ef4e5823feda324aac206a90b53
David Berdik <berdikd@duq.edu>
2019-03-25 22:07:17 -0400

    - Fixed date entry bugs on initial registration activity - Fixed all warnings in "CreateProfileActivity" - Added missing Javadoc comments in "CreateProfileActivity" - Set all variables that can be final to be final in "CreateProfileActivity"

--- app/src/main/java/com/recoveryenhancementsolutions/volition/CreateProfileActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/CreateProfileActivity.java
@@ -1,6 +1,7 @@
 package com.recoveryenhancementsolutions.volition;
 
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,46 +13,84 @@ import java.util.Calendar;
 import java.util.Locale;
 
 /**
- * Class for running activity_create_profile.xml
- * Which includes two pop-up calendars
+ * Class for running activity_create_profile.xml Which includes two pop-up calendars
  */
 public class CreateProfileActivity extends AppCompatActivity {
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_create_profile);
 
-    final Calendar calendar = Calendar.getInstance();
+    final Calendar dobCalendar = Calendar.getInstance();
+    final EditText dateOfBirth = findViewById(R.id.date_of_birth);
+    final Calendar cleanDateCalendar = Calendar.getInstance();
+    final EditText cleanDate = findViewById(R.id.clean_date);
 
-    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+    final DatePickerDialog.OnDateSetListener dateOfBirthListener = new OnDateSetListener() {
+      /**
+       * Event handler for when a date of birth is chosen by the user.
+       * @param view DatePicker object
+       * @param year Year chosen by user
+       * @param month Month chosen by user
+       * @param day Day chosen by user
+       */
       @Override
-      public void onDateSet(DatePicker view, int year, int month, int day) {
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
+      public void onDateSet(final DatePicker view, final int year, final int month, final int day) {
+        dobCalendar.set(Calendar.YEAR, year);
+        dobCalendar.set(Calendar.MONTH, month);
+        dobCalendar.set(Calendar.DAY_OF_MONTH, day);
+        final EditText dob = findViewById(R.id.date_of_birth);
+        dob.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.US).format(dobCalendar.getTime()));
       }
     };
 
-    final EditText dateOfBirth = (EditText) findViewById(R.id.date_of_birth);
-    dateOfBirth.setOnClickListener(new OnClickListener() {
+    final DatePickerDialog.OnDateSetListener cleanDateListener = new OnDateSetListener() {
+      /**
+       * Event handler for when a date of last use is chosen by the user.
+       * @param view DatePicker object
+       * @param year Year chosen by user
+       * @param month Month chosen by user
+       * @param day Day chosen by user
+       */
       @Override
-      public void onClick(View view) {
-        DatePickerDialog pickDate = new DatePickerDialog(CreateProfileActivity.this, date,
-            2000, calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH));
+      public void onDateSet(final DatePicker view, final int year, final int month, final int day) {
+        cleanDateCalendar.set(Calendar.YEAR, year);
+        cleanDateCalendar.set(Calendar.MONTH, month);
+        cleanDateCalendar.set(Calendar.DAY_OF_MONTH, day);
+        final EditText cleanDate = findViewById(R.id.clean_date);
+        cleanDate.setText(
+            new SimpleDateFormat("yyyy-MM-dd", Locale.US).format(cleanDateCalendar.getTime()));
+      }
+    };
+
+    dateOfBirth.setOnClickListener(new OnClickListener() {
+      /**
+       * Event handler for triggering the display of the Date of Birth date picker when the user
+       * taps on the "dateOfBirth" EditText object.
+       * @param view EditText object
+       */
+      @Override
+      public void onClick(final View view) {
+        final DatePickerDialog pickDate = new DatePickerDialog(CreateProfileActivity.this,
+            dateOfBirthListener, 2000, dobCalendar.get(Calendar.MONTH),
+            dobCalendar.get(Calendar.DAY_OF_MONTH));
         pickDate.show();
       }
 
     });
 
-    final EditText cleanDate = (EditText) findViewById(R.id.clean_date);
     cleanDate.setOnClickListener(new OnClickListener() {
+      /**
+       * Event handler for triggering the display of the Last Use Date date picker when the user
+       * taps on the "cleanDate" EditText object.
+       * @param view EditText object
+       */
       @Override
-      public void onClick(View view) {
-        DatePickerDialog pickDate = new DatePickerDialog(CreateProfileActivity.this, date,
-            2019, calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH));
+      public void onClick(final View view) {
+        final DatePickerDialog pickDate = new DatePickerDialog(CreateProfileActivity.this,
+            cleanDateListener, cleanDateCalendar.get(Calendar.YEAR),
+            cleanDateCalendar.get(Calendar.MONTH), cleanDateCalendar.get(Calendar.DAY_OF_MONTH));
         pickDate.show();
       }
 
