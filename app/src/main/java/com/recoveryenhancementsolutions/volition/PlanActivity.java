21d070cf6ea2025ec33d4940524a5d1ef0953d27
Abdulloh Yoldoshev <abdullohyoldoshev@Abdullohs-MacBook-Pro.local>
2019-03-26 14:30:03 -0400

    Redid the xml so it follows the mockup

--- app/src/main/java/com/recoveryenhancementsolutions/volition/PlanActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/PlanActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
-public class ViewActivitiesActivity extends AppCompatActivity {
+public class PlanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_activities);
+        setContentView(R.layout.activity_plan);
     }
 
 }
