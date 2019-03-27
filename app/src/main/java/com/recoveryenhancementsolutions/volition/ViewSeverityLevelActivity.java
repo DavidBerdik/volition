5892a9fe5947da83abe5e04ed437c52ed103ece2
Aidan Hill <hilla4@duq.edu>
2019-03-27 03:53:06 -0400

    began work to add button to next activity

--- app/src/main/java/com/recoveryenhancementsolutions/volition/ViewSeverityLevelActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/ViewSeverityLevelActivity.java
@@ -6,15 +6,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-
 
 
 public class ViewSeverityLevelActivity extends AppCompatActivity {
 
   private TextView specifier;
   private TextView totalYesTv;
+
   /**
    *
    * @param savedInstanceState
@@ -25,17 +26,16 @@ public class ViewSeverityLevelActivity extends AppCompatActivity {
 
     setContentView(R.layout.activity_severitylevel);
 
-specifier= findViewById(R.id.specifier_tv);
-totalYesTv=  findViewById(R.id.totalScore_tv);
-
+    specifier = findViewById(R.id.specifier_tv);
+    totalYesTv = findViewById(R.id.totalScore_tv);
+    Button TPButton = findViewById(R.id.TPButton);
 
     // Get a reference to the ViewModel for this screen.
     mViewModel = ViewModelProviders.of(this).get(ViewSeverityLevelViewModel.class);
-      observeTotalYes();
+    observeTotalYes();
     // Update the UI whenever there's a change in the ViewModel's data.
     observeSeverityLevel();
   }
-
 
   private void observeTotalYes() {
     mViewModel.totalYes.observe(this, new Observer<String>() {
@@ -45,16 +45,15 @@ totalYesTv=  findViewById(R.id.totalScore_tv);
       }
     });
   }
+
   private void showTotalYesInUi(final @NonNull String totalYes) {
     StringBuilder sb = new StringBuilder();
 
-    //sb.append("Severity Level:");
-
     sb.append(totalYes);
-    //sb.append("\n");
 
     totalYesTv.setText(sb.toString());
   }
+
   /**
    *
    */
@@ -70,13 +69,11 @@ totalYesTv=  findViewById(R.id.totalScore_tv);
   private void showSeverityLevelInUi(final @NonNull String severity) {
     StringBuilder sb = new StringBuilder();
 
-
-
     sb.append(severity);
 
-
-  specifier.setText(sb.toString());
+    specifier.setText(sb.toString());
   }
+
   private ViewSeverityLevelViewModel mViewModel;
 
 
