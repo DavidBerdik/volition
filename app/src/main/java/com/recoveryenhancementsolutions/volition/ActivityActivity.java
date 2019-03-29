42fee6fe2e706eca1331e2b46507b39e4f2ad024
Abdulloh Yoldoshev <yoldosheva@duq.edu>
2019-03-29 01:28:08 -0400

    removed buttons, changed weekday letters to be centered (had to make 2 separate xmls so that the textviews were centered in each orientation

--- app/src/main/java/com/recoveryenhancementsolutions/volition/ActivityActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/ActivityActivity.java
@@ -1,5 +1,6 @@
 package com.recoveryenhancementsolutions.volition;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.HorizontalScrollView;
@@ -11,16 +12,27 @@ public class ActivityActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity);
-       String sampletext = "Visit 1 Visit 1";
 
-       final HorizontalScrollView hscroller = findViewById(R.id.hscroller);
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_activity_land);
+        }
+        else
+        setContentView(R.layout.activity_activity_port);
+fillViews();
+    }
+
+    public void fillViews()
+    {
+        String sampletext = "Visit 1 Visit 1";
+
+        final HorizontalScrollView hscroller = findViewById(R.id.hscroller);
         hscroller.post(new Runnable() {
             public void run() {
                 hscroller.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
             }
         });
-     TextView textView = (TextView) findViewById(R.id.textview_day_1);
+        TextView textView = (TextView) findViewById(R.id.textview_day_1);
         textView.setText(sampletext);
         textView = findViewById(R.id.textview_day_2);
         textView.setText(sampletext);
@@ -35,19 +47,18 @@ public class ActivityActivity extends AppCompatActivity {
         textView = findViewById(R.id.textview_day_7);
         textView.setText(sampletext);
         textView = findViewById(R.id.day_of_week_1);
-        textView.setText("M");
+        textView.setText("S");
         textView = findViewById(R.id.day_of_week_2);
-        textView.setText("M");
+        textView.setText("S");
         textView = findViewById(R.id.day_of_week_3);
-        textView.setText("M");
+        textView.setText("F");
         textView = findViewById(R.id.day_of_week_4);
-        textView.setText("M");
+        textView.setText("T");
         textView = findViewById(R.id.day_of_week_5);
         textView.setText("W");
         textView = findViewById(R.id.day_of_week_6);
-        textView.setText("M");
+        textView.setText("T");
         textView = findViewById(R.id.day_of_week_7);
         textView.setText("M");
     }
-
 }
