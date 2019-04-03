2833bdd83fd812b62c09f946a7134fa3c146eab5
Nicholas Costello <costellon1@duq.edu>
2019-04-02 22:08:24 -0400

    Inital setup of test files

---
+++ app/src/androidTest/java/com/recoveryenhancementsolutions/volition/TreatmentExperienceAssessmentActivityTest.java
@@ -0,0 +1,20 @@
+package com.recoveryenhancementsolutions.volition;
+
+import org.junit.*;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TreatmentExperienceAssessmentActivityTest {
+  /**
+  * Creates the test object
+  */
+  @Rule
+  public ActivityTestRule<TreatmentExperienceAssessmentActivity> mActivityTestRule = new ActivityTestRule<>(
+    TreatmentExperienceAssessmentActivity.class);
+
+  @Test
+  public void treatmentExperienceAssessmentActivityTest() {
+
+  }
+
+}
