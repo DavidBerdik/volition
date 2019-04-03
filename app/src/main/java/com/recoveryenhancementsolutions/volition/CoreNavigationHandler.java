8b9db19e7741f5c82c3a92006232952ab43e0ef1
John Natale <natalej1@duq.edu>
2019-04-03 19:46:59 -0400

    Little bit of refractoring.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/CoreNavigationHandler.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/CoreNavigationHandler.java
@@ -1,5 +1,7 @@
 package com.recoveryenhancementsolutions.volition;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
@@ -13,9 +15,34 @@ public class CoreNavigationHandler {
     PAGE_PLAN
   }
 
-  public CoreNavigationHandler(final BottomNavigationView bottomNavigationView) {
-    this.bottomNavigationView = bottomNavigationView;
-    this.bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
+  public CoreNavigationHandler(final BottomNavigationView view, final Context context) {
+    bottomNavigation = view;
+
+    // Create an internal OnNavigationItemSelectedListener.
+    // NOTE: Having it outside this method generated a local-use warning from Android Studio.
+    bottomNavigation.setOnNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(final @NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == bottomNavigation.getSelectedItemId()) {
+          return false;
+        }
+
+        switch (item.getItemId()) {
+          case R.id.core_navigation_home:
+            context.startActivity(new Intent(context, HomeActivity.class));
+            return true;
+          case R.id.core_navigation_activity:
+            context.startActivity(new Intent(context, ReportUseActivity.class));
+            return true;
+          case R.id.core_navigation_plan:
+            context.startActivity(new Intent(context, PlanActivity.class));
+            return true;
+        }
+        return false;
+      }
+    });
   }
 
   /**
@@ -26,31 +53,16 @@ public class CoreNavigationHandler {
   public void setSelectedItem(CoreNavigationPage page) {
     switch (page) {
       case PAGE_HOME:
-        this.bottomNavigationView.setSelectedItemId(R.id.core_navigation_home);
+        this.bottomNavigation.setSelectedItemId(R.id.core_navigation_home);
         break;
       case PAGE_ACTIVITY:
-        this.bottomNavigationView.setSelectedItemId(R.id.core_navigation_activity);
+        this.bottomNavigation.setSelectedItemId(R.id.core_navigation_activity);
         break;
       case PAGE_PLAN:
-        this.bottomNavigationView.setSelectedItemId(R.id.core_navigation_plan);
+        this.bottomNavigation.setSelectedItemId(R.id.core_navigation_plan);
         break;
     }
   }
 
-  private OnNavigationItemSelectedListener navigationListener = new OnNavigationItemSelectedListener() {
-    @Override
-    public boolean onNavigationItemSelected(final @NonNull MenuItem item) {
-      switch (item.getItemId()) {
-        case R.id.core_navigation_home:
-          return true;
-        case R.id.core_navigation_activity:
-          return true;
-        case R.id.core_navigation_plan:
-          return true;
-      }
-      return false;
-    }
-  };
-
-  private final BottomNavigationView bottomNavigationView;
+  private final BottomNavigationView bottomNavigation;
 }
