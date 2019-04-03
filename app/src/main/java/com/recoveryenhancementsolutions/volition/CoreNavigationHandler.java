2b50c231da33119e91788fac77c4d80e30b94b60
John Natale <natalej1@duq.edu>
2019-04-03 19:54:42 -0400

    Add plenty of Javadoc comments.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/CoreNavigationHandler.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/CoreNavigationHandler.java
@@ -7,24 +7,50 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
 import android.view.MenuItem;
 
+/**
+ * The CoreNavigationHandler is a general purpose class intended to be used by multiple activites
+ * provided they all feature the core_navigation menu at the bottom of the device screen.
+ */
 public class CoreNavigationHandler {
 
+  /**
+   * Pages that can be represented by the CoreNavigationHandler.
+   */
   public enum CoreNavigationPage {
+    /**
+     * Represents the HomeActivity.java class.
+     */
     PAGE_HOME,
+
+    /**
+     * Represents the ActivityActivity.java class.
+     */
     PAGE_ACTIVITY,
+
+    /**
+     * Represents the PlanActivity.java class.
+     */
     PAGE_PLAN
   }
 
+  /**
+   * Constructor for the CoreNavigationHandler.
+   *
+   * @param view A BottomNavigationView that should be represented by the core_navigation menu.
+   * @param context The context of the parent activity that will be used to create new intents.
+   */
   public CoreNavigationHandler(final BottomNavigationView view, final Context context) {
     bottomNavigation = view;
 
     // Create an internal OnNavigationItemSelectedListener.
     // NOTE: Having it outside this method generated a local-use warning from Android Studio.
     bottomNavigation.setOnNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+
       @Override
       public boolean onNavigationItemSelected(final @NonNull MenuItem item) {
         int id = item.getItemId();
 
+        // Prevent us from trying to restart the same activity we're already looking at.
         if (id == bottomNavigation.getSelectedItemId()) {
           return false;
         }
@@ -34,6 +60,7 @@ public class CoreNavigationHandler {
             context.startActivity(new Intent(context, HomeActivity.class));
             return true;
           case R.id.core_navigation_activity:
+            // TODO: Update to reflect proper activity once it is added.
             context.startActivity(new Intent(context, ReportUseActivity.class));
             return true;
           case R.id.core_navigation_plan:
