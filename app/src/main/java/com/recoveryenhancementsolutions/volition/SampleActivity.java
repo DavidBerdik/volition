84a864c398cad56dc89ea4c0389b77eb1e10e9af
Stephanie Connell <connells@duq.edu>
2019-04-06 14:48:47 -0400

    Discovered that fragments are the incorrect way to go about adding functionality to this menu.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/SampleActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/SampleActivity.java
@@ -8,7 +8,7 @@ public class SampleActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.sample_activity_layout);
   }
 
 }
