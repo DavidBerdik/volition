c81d5f08c502fab221fe566ac0fb69cb31cdae7c
John Natale <natalej1@duq.edu>
2019-04-10 20:12:12 -0400

    Added NumberPicker to the activity page.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/DailyWellnessActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/DailyWellnessActivity.java
@@ -2,6 +2,9 @@ package com.recoveryenhancementsolutions.volition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 public class DailyWellnessActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class DailyWellnessActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_daily_wellness);
+
+    final NumberPicker np = findViewById(R.id.daily_wellness_number_picker);
+
+    np.setMinValue(1);
+    np.setMaxValue(10);
   }
 }
