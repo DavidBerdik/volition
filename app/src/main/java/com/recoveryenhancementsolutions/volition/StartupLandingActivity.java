8b0eae737dceaaa1acb61aeab1a7e404d6a032b1
John Natale <natalej1@duq.edu>
2019-04-11 15:34:45 -0400

    Add button listener for "Create Profile" button.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/StartupLandingActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/StartupLandingActivity.java
@@ -1,13 +1,40 @@
 package com.recoveryenhancementsolutions.volition;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class StartupLandingActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_startup_landing_port);
+
+    final int orientation = getResources().getConfiguration().orientation;
+    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+      setContentView(R.layout.activity_startup_landing_land);
+    } else {
+      setContentView(R.layout.activity_startup_landing_port);
+    }
+
+    context = this;
+
+    final Button createProfileButton = findViewById(R.id.start_landing_button_create);
+    createProfileButton.setOnClickListener(createProfileButtonListener);
   }
+
+  private OnClickListener createProfileButtonListener = new OnClickListener() {
+
+    @Override
+    public void onClick(final View view) {
+      startActivity(new Intent(context, CreateProfileActivity.class));
+    }
+  };
+
+  private Context context;
 }
