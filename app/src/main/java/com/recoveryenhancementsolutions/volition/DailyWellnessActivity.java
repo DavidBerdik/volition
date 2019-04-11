c26246afa084a4535937ccb94d329bc72c590d01
John Natale <natalej1@duq.edu>
2019-04-10 21:09:45 -0400

    Add functionality to the NumberPicker class.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/DailyWellnessActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/DailyWellnessActivity.java
@@ -2,9 +2,9 @@ package com.recoveryenhancementsolutions.volition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.NumberPicker;
-import android.widget.Spinner;
+import android.widget.NumberPicker.OnValueChangeListener;
+import android.widget.TextView;
 
 public class DailyWellnessActivity extends AppCompatActivity {
 
@@ -17,5 +17,25 @@ public class DailyWellnessActivity extends AppCompatActivity {
     np.setMinValue(1);
     np.setMaxValue(10);
     np.setValue(5);
+    np.setOnValueChangedListener(onValueChangeListener);
+
+    outputs = getResources().getStringArray(R.array.daily_wellness_result_strings);
+    dailyWellnessResultsView = findViewById(R.id.daily_wellness_results);
+    dailyWellnessResultsView.setText(getWellnessString(np.getValue()));
   }
+
+  private String getWellnessString(final int rating) {
+    return getResources().getString(R.string.daily_wellness_rating) + " " + outputs[rating - 1];
+  }
+
+  private OnValueChangeListener onValueChangeListener = new OnValueChangeListener() {
+
+    @Override
+    public void onValueChange(final NumberPicker np, final int oldVal, final int newVal) {
+      dailyWellnessResultsView.setText(getWellnessString(np.getValue()));
+    }
+  };
+
+  private TextView dailyWellnessResultsView;
+  private String[] outputs;
 }
