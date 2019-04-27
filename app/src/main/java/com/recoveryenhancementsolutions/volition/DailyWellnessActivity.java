fdec077bcce5f62b4f91687b762a7fbb0d9ec64b
John Natale <natalej1@duq.edu>
2019-04-27 11:58:29 -0400

    Create basic button listener.

--- app/src/main/java/com/recoveryenhancementsolutions/volition/DailyWellnessActivity.java
+++ app/src/main/java/com/recoveryenhancementsolutions/volition/DailyWellnessActivity.java
@@ -4,9 +4,15 @@ import android.content.res.Configuration;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.NumberPicker.OnValueChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Date;
 
 /**
  * Allows users to report on their daily wellness with a rating from 0 through 10. Users are
@@ -59,6 +65,9 @@ public class DailyWellnessActivity extends AppCompatActivity {
     outputs = getResources().getStringArray(R.array.daily_wellness_result_strings);
     dailyWellnessResultsView = findViewById(R.id.daily_wellness_results);
     dailyWellnessResultsView.setText(getWellnessString(np.getValue()));
+
+    final Button button = findViewById(R.id.daily_wellness_button);
+    button.setOnClickListener(submitButtonListener);
   }
 
   private String getWellnessString(final int rating) {
@@ -71,6 +80,23 @@ public class DailyWellnessActivity extends AppCompatActivity {
     @Override
     public void onValueChange(final NumberPicker np, final int oldVal, final int newVal) {
       dailyWellnessResultsView.setText(getWellnessString(np.getValue()));
+    }
+  };
+
+
+  private OnClickListener submitButtonListener = new OnClickListener() {
+    @Override
+    public void onClick(final View v) {
+      final int rating = lastKnownValue;
+
+      final UserActivityEntity entity = new UserActivityEntity();
+      entity.setDate(new Date());
+
+
+      final Toast toast = Toast
+          .makeText(getApplicationContext(), R.string.daily_wellness_toast, Toast.LENGTH_LONG);
+      toast.setGravity(Gravity.CENTER_VERTICAL, 0, 600);
+      toast.show();
     }
   };
 
