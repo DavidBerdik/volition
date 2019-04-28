41cd37729c6c71a4d2a0e5f131c1c5cabb61d026
John Natale <natalej1@duq.edu>
2019-04-27 23:28:44 -0400

    Update CoreNavigationHandlerTest

--- app/src/androidTest/java/com/recoveryenhancementsolutions/volition/CoreNavigationHandlerTest.java
+++ app/src/androidTest/java/com/recoveryenhancementsolutions/volition/CoreNavigationHandlerTest.java
@@ -1,236 +1,170 @@
 package com.recoveryenhancementsolutions.volition;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.intent.Intents;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.junit.Assert.assertEquals;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import com.recoveryenhancementsolutions.volition.utilities.EspressoTestUtility;
-import org.junit.After;
-import org.junit.Before;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * A testing object used to monitor the functionality of the CoreNavigationHandler. Uses the
- * HomeActivity class as the starting Activity.
+ * Uses the automated Android Studio Espresso recorder to test the CoreNavigationHandler.
  */
+@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class CoreNavigationHandlerTest {
 
   @Rule
-  public final ActivityTestRule<HomeActivity> activityTestRule = new ActivityTestRule<>(
+  public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(
       HomeActivity.class);
 
   /**
-   * Declares a required initialization that allows Espresso to check for intent activity.
-   */
-  @Before
-  public void declareIntent() {
-    Intents.init();
-  }
-
-  /**
-   * Releases a required initialization that allows Espresso to check for intent activity.
-   */
-  @After
-  public void releaseIntent() {
-    Intents.release();
-  }
-
-  /**
-   * Checks that the CoreNavigationHandler properly switches activities once.
+   * The test that checks for all buttons of CoreNavigationHandler to be working.
    */
   @Test
-  public void coreNavigationHandlerTest_Single() {
-    // Confirm that we are on the HomeActivity page.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        HomeActivity.class.getName());
+  public void coreNavigationHandlerTest() {
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), HomeActivity.class.getName());
 
-    // Click to the plan activity.
-    onView(withId(R.id.core_navigation_plan)).perform(click());
+    ViewInteraction bottomNavigationItemView = onView(
+        allOf(withId(R.id.core_navigation_plan), withContentDescription("Plan"),
+            childAtPosition(
+                childAtPosition(
+                    withId(R.id.core_navigation),
+                    0),
+                2),
+            isDisplayed()));
+    bottomNavigationItemView.perform(click());
 
-    // Allow the slower devices/emulators to update.
+    // Added a sleep statement to match the app's execution delay.
+    // The recommended way to handle such scenarios is to use Espresso idling resources:
+    // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
     try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
+      Thread.sleep(700);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
 
-    // Check that we're on the PlanActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        PlanActivity.class.getName());
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), PlanActivity.class.getName());
+
+    pressBack();
+
+    // Added a sleep statement to match the app's execution delay.
+    // The recommended way to handle such scenarios is to use Espresso idling resources:
+    // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+    try {
+      Thread.sleep(700);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), HomeActivity.class.getName());
+
+    ViewInteraction bottomNavigationItemView2 = onView(
+        allOf(withId(R.id.core_navigation_profile), withContentDescription("Profile"),
+            childAtPosition(
+                childAtPosition(
+                    withId(R.id.core_navigation),
+                    0),
+                3),
+            isDisplayed()));
+    bottomNavigationItemView2.perform(click());
+
+    // Added a sleep statement to match the app's execution delay.
+    // The recommended way to handle such scenarios is to use Espresso idling resources:
+    // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+    try {
+      Thread.sleep(700);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), ProfileActivity.class.getName());
+
+    pressBack();
+
+    // Added a sleep statement to match the app's execution delay.
+    // The recommended way to handle such scenarios is to use Espresso idling resources:
+    // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+    try {
+      Thread.sleep(700);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), HomeActivity.class.getName());
+
+    ViewInteraction bottomNavigationItemView3 = onView(
+        allOf(withId(R.id.core_navigation_activity), withContentDescription("Activity"),
+            childAtPosition(
+                childAtPosition(
+                    withId(R.id.core_navigation),
+                    0),
+                1),
+            isDisplayed()));
+    bottomNavigationItemView3.perform(click());
+
+    // Added a sleep statement to match the app's execution delay.
+    // The recommended way to handle such scenarios is to use Espresso idling resources:
+    // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+    try {
+      Thread.sleep(700);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), ActivityActivity.class.getName());
+
+    pressBack();
+
+    // Added a sleep statement to match the app's execution delay.
+    // The recommended way to handle such scenarios is to use Espresso idling resources:
+    // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+    try {
+      Thread.sleep(700);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(), HomeActivity.class.getName());
   }
 
-  /**
-   * Checks that the CoreNavigationHandler properly handles multiple activities at once.
-   */
-  @Test
-  public void coreNavigationHandlerTest_Multiple() {
-    // Confirm that we are on the HomeActivity page.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        HomeActivity.class.getName());
+  private static Matcher<View> childAtPosition(
+      final Matcher<View> parentMatcher, final int position) {
 
-    // Click to the profile activity.
-    onView(withId(R.id.core_navigation_profile)).perform(click());
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("Child at position " + position + " in parent ");
+        parentMatcher.describeTo(description);
+      }
 
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the ProfileActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        ProfileActivity.class.getName());
-
-    // Click to the plan activity.
-    onView(withId(R.id.core_navigation_plan)).perform(click());
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the PlanActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        PlanActivity.class.getName());
-
-    // Click to the Activity activity.
-    onView(withId(R.id.core_navigation_activity)).perform(click());
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the ActivityActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        ActivityActivity.class.getName());
-
-    // Click to the Activity activity (should not function).
-    onView(withId(R.id.core_navigation_activity)).perform(click());
-
-    // Check that we're on the ActivityActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        ActivityActivity.class.getName());
-
-    Espresso.pressBack();
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the PlanActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        PlanActivity.class.getName());
-
-    Espresso.pressBack();
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the ProfileActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        ProfileActivity.class.getName());
-
-    Espresso.pressBack();
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the HomeActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        HomeActivity.class.getName());
-
-    // Click to the Activity activity.
-    onView(withId(R.id.core_navigation_activity)).perform(click());
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the ActivityActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        ActivityActivity.class.getName());
-
-    // Click to the Plan activity.
-    onView(withId(R.id.core_navigation_plan)).perform(click());
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the PlanActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        PlanActivity.class.getName());
-
-    // Click to the Profile activity.
-    onView(withId(R.id.core_navigation_profile)).perform(click());
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the ProfileActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        ProfileActivity.class.getName());
-
-    Espresso.pressBack();
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the PlanActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        PlanActivity.class.getName());
-
-    // Click to the Home activity.
-    onView(withId(R.id.core_navigation_home)).perform(click());
-
-    // Allow the slower devices/emulators to update.
-    try {
-      Thread.sleep(1000);
-    } catch (final InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-
-    // Check that we're on the HomeActivity class.
-    assertEquals(EspressoTestUtility.getCurrentActivity().getClass().getName(),
-        HomeActivity.class.getName());
+      @Override
+      public boolean matchesSafely(View view) {
+        ViewParent parent = view.getParent();
+        return parent instanceof ViewGroup && parentMatcher.matches(parent)
+            && view.equals(((ViewGroup) parent).getChildAt(position));
+      }
+    };
   }
 }
